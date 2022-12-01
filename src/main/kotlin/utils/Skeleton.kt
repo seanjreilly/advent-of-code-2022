b57e38e182c6633df8ae29555e67a15e14ef63fb
Sean Reilly <seanjreilly@gmail.com>
2022-12-01 10:20:39 +0000

    Add util for reading input file and a daily skeleton

---
+++ src/main/kotlin/utils/Skeleton.kt
@@ -0,0 +1,15 @@
+package utils
+
+fun main() {
+    val input = readInput("DayXX")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Long {
+    return 0
+}
+
+fun part2(input: List<String>): Long {
+    return 0
+}
