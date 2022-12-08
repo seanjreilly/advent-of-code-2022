f2b87217a4534798f6f4522b21826a3b57d24cd1
Sean Reilly <seanjreilly@gmail.com>
2022-12-08 15:19:37 +0000

    Move to a more straightforward solution for part 1.
    This approach could be considered more naive from a performance perspective,
    but it still returns a result near-instantly, and it's much shorter and
    easier to understand.

--- src/main/kotlin/day08/Day08.kt
+++ src/main/kotlin/day08/Day08.kt
@@ -3,7 +3,6 @@ package day08
 import utils.gridmap.GridMap
 import utils.gridmap.Point
 import utils.readInput
-import kotlin.math.max
 
 fun main() {
     val input = readInput("Day08")
@@ -23,75 +22,28 @@ fun part2(input: List<String>): Int {
 class ForestMap(rawData: List<String>) : GridMap<Int>(parseTrees(rawData), Point::getCardinalNeighbours) {
 
     fun findVisibleTrees(): Set<Point> {
-        /*
-         * Start by marking each tree on the edge visible (nothing blocking the view of these trees)
-         * Then sweep away from the edge. Each tree with a visible neighbour that is taller than the
-         * largest tree seen in the row or column so far is visible
-         *
-         * Do this for each edge and then combine the results
-         */
-        val result = mutableSetOf<Point>()
-        var highestTreeSoFar: IntArray
 
-        //sweep south from the north edge
-        (0 until width).forEach { result += Point(it, 0) } //explicitly add the trees on the edge in case one is zero units tall
-        highestTreeSoFar = IntArray(width) { this[Point(it, 0)] }
-        (1 until height).forEach { y ->
-            (0 until width).forEach {x ->
-                val point = Point(x,y)
-                val height = this[point]
-                if (height > highestTreeSoFar[x]) {
-                    result += point
+        fun visible(point: Point, move: (Point) -> Point): Boolean {
+            val treeHeight = this[point]
+            var pt = move.invoke(point)
+            while (contains(pt)) {
+                if (this[pt] >= treeHeight) {
+                    return false
                 }
-                highestTreeSoFar[x] = max(highestTreeSoFar[x], height)
+                pt = move.invoke(pt)
             }
+            return true
         }
 
-        //sweep north from the south edge
-        (0 until width).forEach { result += Point(it, height -1) } //explicitly add the trees on the edge in case one is zero height
-        highestTreeSoFar = IntArray(width) { this[Point(it, height -1)] }
-        (height - 2 downTo 0).forEach { y ->
-            (0 until width).forEach {x ->
-                val point = Point(x,y)
-                val height = this[point]
-                if (height > highestTreeSoFar[x]) {
-                    result += point
-                }
-                highestTreeSoFar[x] = max(highestTreeSoFar[x], height)
+        return iterator()
+            .asSequence()
+            .filter { point ->
+                visible(point, Point::north) ||
+                    visible(point, Point::east) ||
+                    visible(point, Point::south) ||
+                    visible(point, Point::west)
             }
-        }
-
-
-
-        //sweep east from the west edge
-        (0 until height).forEach { result += Point(0, it) } //explicitly add the trees on the edge in case one is zero units tall
-        highestTreeSoFar = IntArray(height) { this[Point(0, it)] }
-        (0 until width).forEach { x ->
-            (0 until height).forEach {y ->
-                val point = Point(x,y)
-                val height = this[point]
-                if (height > highestTreeSoFar[y]) {
-                    result += point
-                }
-                highestTreeSoFar[y] = max(highestTreeSoFar[y], height)
-            }
-        }
-
-
-        //sweep west from the east edge
-        (0 until height).forEach { result += Point(width -1, it) } //explicitly add the trees on the edge in case one is zero units tall
-        highestTreeSoFar = IntArray(height) { this[Point(width -1, it)] }
-        (width - 2 downTo 0).forEach { x ->
-            (0 until height).forEach { y ->
-                val point = Point(x,y)
-                val height = this[point]
-                if (height > highestTreeSoFar[y]) {
-                    result += point
-                }
-                highestTreeSoFar[y] = max(highestTreeSoFar[y], height)
-            }
-        }
-        return result
+            .toSet()
     }
 
     fun getSenicScore(point: Point): Int {
