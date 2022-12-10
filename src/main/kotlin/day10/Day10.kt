68cefc4e979e820b9fdbe0dc88d01c7dc115c7cd
Sean Reilly <seanjreilly@gmail.com>
2022-12-10 13:05:58 +0000

    Day 10, part 1
    Use sequences to emulate the logic, and then pick the
    values out of the sequence and sum them.

---
+++ src/main/kotlin/day10/Day10.kt
@@ -0,0 +1,45 @@
+package day10
+
+import utils.readInput
+
+fun main() {
+    val input = readInput("Day10")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Long {
+    val cyclesToSample = setOf(20, 60, 100, 140, 180, 220).map { it - 1 }.toSet() //make it zero-based
+    return calculateSignalStrengths(input)
+        .filterIndexed { index, signalStrength -> index in cyclesToSample }
+        .map { it.toLong() }
+        .sum()
+
+}
+
+fun part2(input: List<String>): Long {
+    return 0
+}
+
+fun calculateSignalStrengths(input: List<String>) = sequence {
+    calculateRegisterValues(input)
+        .forEachIndexed { index, registerValue ->
+            yield(registerValue * (index + 1))
+        }
+}
+
+fun calculateRegisterValues(input: List<String>): Sequence<Int> = sequence {
+    var registerValue = 1 //the register always starts at 1
+    input.forEach { line ->
+        if (line == "noop") { //noop instruction
+            yield(registerValue) //the value doesn't change during or after the cycle for a noop
+            return@forEach
+        }
+        //addX instruction
+        val incrementValue = line.substring(5).toInt()
+        yield(registerValue) //the register doesn't change during the first cycle
+        yield(registerValue) //or during the second cycle, only at the end of the cycle (will be output next time)
+        registerValue += incrementValue
+    }
+    yield(registerValue) //the register value at the end of the final cycle
+}
---
+++ src/test/kotlin/day10/Day10Test.kt
@@ -0,0 +1,180 @@
+package day10
+
+import org.junit.jupiter.api.Test
+
+class Day10Test {
+    private val sampleInput = """
+        noop
+        addx 3
+        addx -5
+    """.trimIndent().lines()
+
+    private val largerInput = """
+        addx 15
+        addx -11
+        addx 6
+        addx -3
+        addx 5
+        addx -1
+        addx -8
+        addx 13
+        addx 4
+        noop
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx 5
+        addx -1
+        addx -35
+        addx 1
+        addx 24
+        addx -19
+        addx 1
+        addx 16
+        addx -11
+        noop
+        noop
+        addx 21
+        addx -15
+        noop
+        noop
+        addx -3
+        addx 9
+        addx 1
+        addx -3
+        addx 8
+        addx 1
+        addx 5
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx -36
+        noop
+        addx 1
+        addx 7
+        noop
+        noop
+        noop
+        addx 2
+        addx 6
+        noop
+        noop
+        noop
+        noop
+        noop
+        addx 1
+        noop
+        noop
+        addx 7
+        addx 1
+        noop
+        addx -13
+        addx 13
+        addx 7
+        noop
+        addx 1
+        addx -33
+        noop
+        noop
+        noop
+        addx 2
+        noop
+        noop
+        noop
+        addx 8
+        noop
+        addx -1
+        addx 2
+        addx 1
+        noop
+        addx 17
+        addx -9
+        addx 1
+        addx 1
+        addx -3
+        addx 11
+        noop
+        noop
+        addx 1
+        noop
+        addx 1
+        noop
+        noop
+        addx -13
+        addx -19
+        addx 1
+        addx 3
+        addx 26
+        addx -30
+        addx 12
+        addx -1
+        addx 3
+        addx 1
+        noop
+        noop
+        noop
+        addx -9
+        addx 18
+        addx 1
+        addx 2
+        noop
+        noop
+        addx 9
+        noop
+        noop
+        noop
+        addx -1
+        addx 2
+        addx -37
+        addx 1
+        addx 3
+        noop
+        addx 15
+        addx -21
+        addx 22
+        addx -6
+        addx 1
+        noop
+        addx 2
+        addx 1
+        noop
+        addx -10
+        noop
+        noop
+        addx 20
+        addx 1
+        addx 2
+        addx 2
+        addx -6
+        addx -11
+        noop
+        noop
+        noop
+    """.trimIndent().lines()
+
+    @Test
+    fun `part1 should return the sum of the signal strengths during cycles 20, 60, 100, 140, 180, and 220`() {
+        assert(part1(largerInput) == 13140L)
+    }
+    
+    @Test
+    fun `calculateSignalStrengths should return the cycle number times the register value`() {
+        val result: List<Int> = calculateSignalStrengths(sampleInput).toList()
+
+        assert(result == listOf(1, 2, 3, 16, 20, -6))
+    }
+
+    @Test
+    fun `calculateRegisterValues should return the value of the register during every cycle`() {
+        //remember this is zero-based, so the value of the register during the 1st cycle is in the zeroth slot
+        val result: List<Int> = calculateRegisterValues(sampleInput).toList()
+
+        assert(result == listOf(1,1,1,4,4,-1))
+    }
+}
