1203af7cfb465dadf51e2707456ce9e54ea1ea91
Sean Reilly <seanjreilly@gmail.com>
2022-12-11 11:36:54 +0000

    Day 11, part 1
    Build an item class, a monkey class, an expression class
    and parse functions for all of these
    
    Then run the considerItems 20 times and track the number of items each
    monkey considers
    
    Calculate the part1 answer by running a stream operation, sorting, taking
    the top 2 results, and multiplying them.

---
+++ src/main/kotlin/day11/Day11.kt
@@ -0,0 +1,155 @@
+package day11
+
+import utils.readInput
+
+fun main() {
+    val input = readInput("Day11")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Long {
+    val monkeys = parseMonkeys(input)
+    processRounds(monkeys)
+    return monkeys
+        .map { it.totalNumberOfItemsConsidered }
+        .sortedDescending()
+        .take(2)
+        .reduce(Long::times)
+}
+
+fun part2(input: List<String>): Long {
+    return 0
+}
+
+enum class Operator(val operation: (Long, Long) -> Long) {
+    PLUS(Long::plus),
+    TIMES(Long::times)
+}
+
+data class SimpleExpression(val first: Operand, val operator: Operator, val second: Operand) {
+    fun evaluate(item: Item): Long {
+        return operator.operation.invoke(first.getValue(item), second.getValue(item))
+    }
+
+    companion object {
+        private val parseRegex = """(old|\d+) ([*+]) (old|\d+)""".toRegex()
+        fun parse(rawExpression: String): SimpleExpression {
+            val (rawFirstOperand, rawOperator, rawSecondOperand) = parseRegex.matchEntire(rawExpression)!!.destructured
+            val operator = when (rawOperator) {
+                "+" -> Operator.PLUS
+                "*" -> Operator.TIMES
+                else -> { throw IllegalArgumentException("Unexpected operator ${rawOperator}") }
+            }
+            return SimpleExpression(parseOperand(rawFirstOperand), operator, parseOperand(rawSecondOperand))
+        }
+
+        private fun parseOperand(raw: String) : Operand {
+            return when (raw) {
+                "old" -> Old
+                else -> {
+                    Constant(raw.toLong())
+                }
+            }
+        }
+    }
+}
+
+//region operands
+sealed interface Operand {
+    fun getValue(item: Item): Long
+}
+
+data class Constant(private val value: Long) : Operand {
+    override fun getValue(item: Item) = value
+}
+
+object Old : Operand {
+    override fun getValue(item: Item) = item.worryLevel
+}
+//endregion
+
+data class Item(var worryLevel: Long)
+
+fun parseMonkeys(input: List<String>): List<Monkey> {
+    return input
+        .windowed(7, 7, true) //the last monkey doesn't have a trailing blank line
+        .map { chunk -> parseMonkey(chunk) }
+}
+
+private fun parseMonkey(chunk: List<String>): Monkey {
+    //ignore the first line — it just contains the monkey id, which is always the same as the position, so we don't need it
+    /*
+        The general pattern is to trim each line to avoid indentation,
+        skip the prefix for each line with substring,
+        and then process the remainder
+     */
+    val rawItemWorryLevels = chunk[1]
+        .trim()
+        .substring("Starting items: ".length)
+        .split(',')
+        .map { it.trim().toInt() }
+    val rawExpression = chunk[2]
+        .trim()
+        .substring("Operation: new = ".length)
+    val divisibleBy = chunk[3]
+        .trim()
+        .substring("Test: divisible by ".length)
+        .toInt()
+    val testPassesIndex = chunk[4]
+        .trim()
+        .substring("If true: throw to monkey ".length)
+        .toInt()
+    val testFailsIndex = chunk[5]
+        .trim()
+        .substring("If false: throw to monkey ".length)
+        .toInt()
+    return Monkey(rawItemWorryLevels, rawExpression, divisibleBy, testPassesIndex, testFailsIndex)
+}
+
+data class Monkey(
+    val items: MutableList<Item>, //the starting items the monkey has
+    val operation: SimpleExpression, //the operation expression — how worry is affected when the monkey examines an item
+    val divisibleBy: Int, //the test is always "is divisible by a divisor", so just track the divisor
+    val testPassesIndex: Int, //the index of the monkey the item is passed to if the test succeeds
+    val testFailsIndex: Int //the index of the monkey the item is passed to if the test fails
+) {
+    fun considerItems(): List<Pair<Item, Int>> {
+        val oldItems = items.toList()
+        totalNumberOfItemsConsidered += oldItems.size
+        items.clear()
+        return oldItems
+            .map { oldItem ->
+                var worryLevel = operation.evaluate(oldItem) //edit the worry level by inspection
+                worryLevel /= 3 // the worry level naturally decreases when the item isn't broken
+                Item(worryLevel)
+            }
+            .map {
+                val destination = if (it.worryLevel % divisibleBy == 0L) { testPassesIndex } else { testFailsIndex }
+                Pair(it, destination)
+            }
+            .toList()
+    }
+
+    var totalNumberOfItemsConsidered = 0L
+        private set
+
+    constructor(itemWorryLevels: List<Int>, rawOperation: String, divisibleBy:Int, testPassesIndex: Int, testFailsIndex: Int):
+        this(
+            itemWorryLevels.map { Item(it.toLong()) }.toMutableList(),
+            SimpleExpression.parse(rawOperation),
+            divisibleBy,
+            testPassesIndex,
+            testFailsIndex
+        )
+}
+
+fun processRounds(monkeys: List<Monkey>) {
+    repeat(20) {
+        monkeys.forEach { monkey ->
+            monkey.considerItems().forEach { (item, destinationMonkey) ->
+                monkeys[destinationMonkey].items.add(item)
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/day11/Day11Test.kt
@@ -0,0 +1,165 @@
+package day11
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class Day11Test {
+    val sampleInput = """
+        Monkey 0:
+          Starting items: 79, 98
+          Operation: new = old * 19
+          Test: divisible by 23
+            If true: throw to monkey 2
+            If false: throw to monkey 3
+        
+        Monkey 1:
+          Starting items: 54, 65, 75, 74
+          Operation: new = old + 6
+          Test: divisible by 19
+            If true: throw to monkey 2
+            If false: throw to monkey 0
+        
+        Monkey 2:
+          Starting items: 79, 60, 97
+          Operation: new = old * old
+          Test: divisible by 13
+            If true: throw to monkey 1
+            If false: throw to monkey 3
+        
+        Monkey 3:
+          Starting items: 74
+          Operation: new = old + 3
+          Test: divisible by 17
+            If true: throw to monkey 0
+            If false: throw to monkey 1
+    """.trimIndent().lines()
+
+    @Test
+    fun `processRounds should pass items from monkey to monkey for 20 rounds`() {
+        val monkeys = parseMonkeys(sampleInput)
+        processRounds(monkeys)
+
+        assert(monkeys[0].items == listOf(10, 12, 14, 26, 34).map { it.toLong() }.map(::Item))
+        assert(monkeys[1].items == listOf(245, 93, 53, 199, 115).map { it.toLong() }.map(::Item))
+        assert(monkeys[2].items == emptyList<Item>())
+        assert(monkeys[3].items == emptyList<Item>())
+
+        assert(monkeys[0].totalNumberOfItemsConsidered == 101L)
+        assert(monkeys[1].totalNumberOfItemsConsidered == 95L)
+        assert(monkeys[2].totalNumberOfItemsConsidered == 7L)
+        assert(monkeys[3].totalNumberOfItemsConsidered == 105L)
+    }
+
+    @Test
+    fun `part1 should pass items from monkey to monkey for 20 rounds and then return the product of the largest 2 numbers of items considered`() {
+        assert(part1(sampleInput) == 10605L)
+    }
+
+
+    @Test
+    fun `parseMonkeys should return a list of Monkey instances`() {
+        val monkeys: List<Monkey> = parseMonkeys(sampleInput)
+
+        val expectedResult = listOf(
+            Monkey(listOf(79,98),"old * 19", 23, 2, 3),
+            Monkey(listOf(54, 65, 75, 74), "old + 6", 19, 2, 0),
+            Monkey(listOf(79, 60, 97), "old * old", 13, 1, 3),
+            Monkey(listOf(74), "old + 3", 17, 0, 1)
+        )
+
+        assert(monkeys.size == 4)
+        assert(monkeys.first() == expectedResult.first())
+        assert(monkeys[1] == expectedResult[1])
+        assert(monkeys[2] == expectedResult[2])
+        assert(monkeys[3] == expectedResult[3])
+    }
+
+    @Nested
+    inner class SimpleExpressionTest {
+        @Test
+        fun `parse should return an addition expression given an expression in String form`() {
+            val rawExpression = "old + 3"
+            val result = SimpleExpression.parse(rawExpression)
+
+            val expectedResult = SimpleExpression(Old, Operator.PLUS, Constant(3))
+            assert(expectedResult == result)
+        }
+
+        @Test
+        fun `parse should successfully parse all expressions in the simple example`() {
+            assert(SimpleExpression.parse("old * 19") == SimpleExpression(Old, Operator.TIMES, Constant(19)))
+            assert(SimpleExpression.parse("old + 6") == SimpleExpression(Old, Operator.PLUS, Constant(6)))
+            assert(SimpleExpression.parse("old * old") == SimpleExpression(Old, Operator.TIMES, Old))
+            assert(SimpleExpression.parse("old + 3") == SimpleExpression(Old, Operator.PLUS, Constant(3)))
+        }
+
+        @Test
+        fun `evaluate should correctly evaluate an expression given a value`() {
+            val item = Item(2)
+            assert(SimpleExpression(Old, Operator.TIMES, Constant(19)).evaluate(item) == 38L)
+            assert(SimpleExpression(Old, Operator.PLUS, Constant(6)).evaluate(item) == 8L)
+            assert(SimpleExpression(Old, Operator.TIMES, Old).evaluate(item) == 4L)
+            assert(SimpleExpression(Old, Operator.PLUS, Constant(3)).evaluate(item) == 5L)
+
+            //and try one with a different item just to be sure
+            assert(SimpleExpression(Old, Operator.TIMES, Old).evaluate(Item(9)) == 81L)
+        }
+    }
+
+    @Nested
+    inner class MonkeyTest {
+        @Test
+        fun `considerItems should return a list of modified items and destination monkeys`() {
+            val monkey = Monkey(
+                listOf(54, 65, 75, 51), //modify Monkey 1 from the example slightly so we trigger it's true destination
+                "old + 6",
+                19,
+                2,
+                0
+            )
+
+            val results:List<Pair<Item, Int>> = monkey.considerItems()
+
+            val expectedResults = listOf(
+                Pair(Item(20), 0),
+                Pair(Item(23), 0),
+                Pair(Item(27), 0),
+                Pair(Item(19), 2)
+            )
+
+            assert(results.size == expectedResults.size)
+            expectedResults.forEachIndexed { index, expectedResult ->
+                assert(expectedResult.first == results[index].first)
+                assert(expectedResult.second == results[index].second)
+            }
+        }
+
+        @Test
+        fun `considerItems should clear the current items the monkey has`() {
+            val monkey = parseMonkeys(sampleInput)[1]
+            assert(monkey.items.isNotEmpty()) { "precondition: the monkey must have items before we consider"}
+
+            monkey.considerItems()
+
+            assert(monkey.items.isEmpty())
+        }
+
+        @Test
+        fun `considerItems should increment the total number of items this monkey has considered every time it is called`() {
+            val monkey = parseMonkeys(sampleInput)[1]
+            val initialItemsSize = monkey.items.size
+            assert(initialItemsSize > 0) { "precondition: the monkey must have items before we consider"}
+
+            monkey.considerItems()
+
+            assert(monkey.totalNumberOfItemsConsidered == initialItemsSize.toLong())
+
+            //give the monkey two more items
+            val extraItems = listOf(Item(1), Item(2))
+            monkey.items.addAll(extraItems)
+
+            monkey.considerItems()
+            assert(monkey.totalNumberOfItemsConsidered == (initialItemsSize + extraItems.size).toLong())
+        }
+    }
+}
