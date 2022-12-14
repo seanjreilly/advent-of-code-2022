8d6ae44180aa0b5d4e451f358187fe7cbd625dec
Sean Reilly <seanjreilly@gmail.com>
2022-12-14 14:52:51 +0000

    Add extra test
    Simplifying the two loops is going to be annoying (they
    don't just have different predicates — one has an extra
    break statement that the other doesn't have for points that
    "fall to the abyss").
    
    So, rather than unifying the two loops into a single
    function, add a big old test that asserts that the state
    for part 2's function matches the requirements.

--- src/test/kotlin/day14/Day14Test.kt
+++ src/test/kotlin/day14/Day14Test.kt
@@ -103,5 +103,129 @@ class Day14Test {
             val result = cave.simulateSandWithFloor()
             assert(result == 93)
         }
+
+        @Test
+        fun `simulateSandWithFloor should include grains of sand that have come to rest in blockedSquares`() {
+            """
+                ............o............
+                ...........ooo...........
+                ..........ooooo..........
+                .........ooooooo.........
+                ........oo#ooo##o........
+                .......ooo#ooo#ooo.......
+                ......oo###ooo#oooo......
+                .....oooo.oooo#ooooo.....
+                ....oooooooooo#oooooo....
+                ...ooo#########ooooooo...
+                ..ooooo.......ooooooooo..
+                #########################
+            """.trimIndent()
+
+            val cave = Cave(sampleInput)
+            val initialBlockedSquares = cave.blockedSquares.size
+
+            val result = cave.simulateSandWithFloor()
+            assert(cave.blockedSquares.size == initialBlockedSquares + result)
+
+            assert(Point(490,10) in cave.blockedSquares)
+            assert(Point(491,10) in cave.blockedSquares)
+            assert(Point(492,10) in cave.blockedSquares)
+            assert(Point(493,10) in cave.blockedSquares)
+            assert(Point(494,10) in cave.blockedSquares)
+            assert(Point(502,10) in cave.blockedSquares)
+            assert(Point(503,10) in cave.blockedSquares)
+            assert(Point(503,10) in cave.blockedSquares)
+            assert(Point(504,10) in cave.blockedSquares)
+            assert(Point(505,10) in cave.blockedSquares)
+            assert(Point(506,10) in cave.blockedSquares)
+            assert(Point(507,10) in cave.blockedSquares)
+            assert(Point(508,10) in cave.blockedSquares)
+            assert(Point(509,10) in cave.blockedSquares)
+            assert(Point(510,10) in cave.blockedSquares)
+
+            assert(Point(491,9) in cave.blockedSquares)
+            assert(Point(492,9) in cave.blockedSquares)
+            assert(Point(493,9) in cave.blockedSquares)
+            assert(Point(503,9) in cave.blockedSquares)
+            assert(Point(504,9) in cave.blockedSquares)
+            assert(Point(505,9) in cave.blockedSquares)
+            assert(Point(506,9) in cave.blockedSquares)
+            assert(Point(507,9) in cave.blockedSquares)
+            assert(Point(508,9) in cave.blockedSquares)
+            assert(Point(509,9) in cave.blockedSquares)
+
+            assert(Point(492,8) in cave.blockedSquares)
+            assert(Point(493,8) in cave.blockedSquares)
+            assert(Point(494,8) in cave.blockedSquares)
+            assert(Point(495,8) in cave.blockedSquares)
+            assert(Point(496,8) in cave.blockedSquares)
+            assert(Point(497,8) in cave.blockedSquares)
+            assert(Point(498,8) in cave.blockedSquares)
+            assert(Point(499,8) in cave.blockedSquares)
+            assert(Point(500,8) in cave.blockedSquares)
+            assert(Point(501,8) in cave.blockedSquares)
+
+            assert(Point(493,7) in cave.blockedSquares)
+            assert(Point(494,7) in cave.blockedSquares)
+            assert(Point(495,7) in cave.blockedSquares)
+            assert(Point(496,7) in cave.blockedSquares)
+            assert(Point(498,7) in cave.blockedSquares)
+            assert(Point(499,7) in cave.blockedSquares)
+            assert(Point(500,7) in cave.blockedSquares)
+            assert(Point(501,7) in cave.blockedSquares)
+            assert(Point(503,7) in cave.blockedSquares)
+            assert(Point(504,7) in cave.blockedSquares)
+            assert(Point(505,7) in cave.blockedSquares)
+            assert(Point(506,7) in cave.blockedSquares)
+            assert(Point(507,7) in cave.blockedSquares)
+
+            assert(Point(494,6) in cave.blockedSquares)
+            assert(Point(495,6) in cave.blockedSquares)
+            assert(Point(499,6) in cave.blockedSquares)
+            assert(Point(500,6) in cave.blockedSquares)
+            assert(Point(501,6) in cave.blockedSquares)
+            assert(Point(503,6) in cave.blockedSquares)
+            assert(Point(504,6) in cave.blockedSquares)
+            assert(Point(505,6) in cave.blockedSquares)
+            assert(Point(506,6) in cave.blockedSquares)
+
+            assert(Point(495,5) in cave.blockedSquares)
+            assert(Point(496,5) in cave.blockedSquares)
+            assert(Point(497,5) in cave.blockedSquares)
+            assert(Point(499,5) in cave.blockedSquares)
+            assert(Point(500,5) in cave.blockedSquares)
+            assert(Point(501,5) in cave.blockedSquares)
+            assert(Point(503,5) in cave.blockedSquares)
+            assert(Point(504,5) in cave.blockedSquares)
+            assert(Point(505,5) in cave.blockedSquares)
+
+            assert(Point(496,4) in cave.blockedSquares)
+            assert(Point(497,4) in cave.blockedSquares)
+            assert(Point(499,4) in cave.blockedSquares)
+            assert(Point(500,4) in cave.blockedSquares)
+            assert(Point(501,4) in cave.blockedSquares)
+            assert(Point(503,4) in cave.blockedSquares)
+            assert(Point(504,4) in cave.blockedSquares)
+
+            assert(Point(497,3) in cave.blockedSquares)
+            assert(Point(498,3) in cave.blockedSquares)
+            assert(Point(499,3) in cave.blockedSquares)
+            assert(Point(500,3) in cave.blockedSquares)
+            assert(Point(501,3) in cave.blockedSquares)
+            assert(Point(502,3) in cave.blockedSquares)
+            assert(Point(503,3) in cave.blockedSquares)
+
+            assert(Point(498,2) in cave.blockedSquares)
+            assert(Point(499,2) in cave.blockedSquares)
+            assert(Point(500,2) in cave.blockedSquares)
+            assert(Point(501,2) in cave.blockedSquares)
+            assert(Point(502,2) in cave.blockedSquares)
+
+            assert(Point(499,1) in cave.blockedSquares)
+            assert(Point(500,1) in cave.blockedSquares)
+            assert(Point(501,1) in cave.blockedSquares)
+
+            assert(Point(500,0) in cave.blockedSquares)
+        }
     }
 }
