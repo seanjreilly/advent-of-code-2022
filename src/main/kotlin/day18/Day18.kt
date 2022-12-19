5551e41a675d6942eed9e07786d15f2e87758372
Sean Reilly <seanjreilly@gmail.com>
2022-12-19 00:28:39 +0000

    Day 18, part 2
    A correct answer, but slow (~8 seconds compute time).
    Find the interior points and add them, then calculate
    the solution as before.
    
    Enumerate interior points by brute forcing every point
    in the bounding box and checking if it's an interior
    point.
    
    Find interior points by BFS through neighbours (pruning
    results that hit known points) until a point outside the
    bounding is reached, or not. Points that can't reach
    outside the bounding box are interior points.

--- src/main/kotlin/day18/Day18.kt
+++ src/main/kotlin/day18/Day18.kt
@@ -17,8 +17,10 @@ fun part1(input: List<String>): Int {
     return countUnconnectedSides(parse(input))
 }
 
-fun part2(input: List<String>): Long {
-    return 0
+fun part2(input: List<String>): Int {
+    val points = parse(input)
+    val interiorPoints = findInteriorPoints(points)
+    return countUnconnectedSides(points + interiorPoints)
 }
 
 fun countUnconnectedSides(points: Set<Point3D>): Int {
@@ -26,6 +28,48 @@ fun countUnconnectedSides(points: Set<Point3D>): Int {
         val neighboursThatExist = point.neighbours().filter { it in points }
         6 - neighboursThatExist.size
     }
+}
+
+fun findInteriorPoints(points: Set<Point3D>): Set<Point3D> {
+    //calculate bounding box
+    val xBounds = points.minOf { it.x }..points.maxOf { it.x }
+    val yBounds = points.minOf { it.y }..points.maxOf { it.y }
+    val zBounds = points.minOf { it.z }..points.maxOf { it.z }
+
+    return xBounds
+        .flatMap { x ->
+            yBounds.flatMap { y ->
+                zBounds.map { z -> Point3D(x, y, z) }
+            }
+        }
+        .filter { it !in points }
+        .filter { isInteriorPoint(it, points) }
+        .toSet()
+}
+
+fun isInteriorPoint(potentialPoint: Point3D, points: Set<Point3D>): Boolean {
+    //calculate bounding box
+    val xBounds = points.minOf { it.x }..points.maxOf { it.x }
+    val yBounds = points.minOf { it.y }..points.maxOf { it.y }
+    val zBounds = points.minOf { it.z }..points.maxOf { it.z }
+
+    val alreadyChecked = mutableSetOf<Point3D>()
+    val queue = ArrayDeque<Point3D>()
+    queue.addLast(potentialPoint)
+    while (queue.isNotEmpty()) {
+        val point = queue.removeFirst()
+        if (point in alreadyChecked) {
+            continue
+        }
+
+        alreadyChecked += point
+
+        if (point.x !in xBounds && point.y !in yBounds && point.z !in zBounds) {
+            return false //we've found the outside
+        }
+        point.neighbours().filter { it !in points }.forEach { queue.addLast(it) }
+    }
+    return true
 }
 
 fun parse(input: List<String>): Set<Point3D> {
--- src/test/kotlin/day18/Day18Test.kt
+++ src/test/kotlin/day18/Day18Test.kt
@@ -26,6 +26,11 @@ class Day18Test {
     }
 
     @Test
+    fun `part2 should fill in any interior points, and then count the unconnected sides for the input`() {
+        assert(part2(sampleInput) == 58)
+    }
+
+    @Test
     fun `parse should return a list of Point3D instances`() {
         val points = parse(sampleInput)
 
@@ -72,6 +77,179 @@ class Day18Test {
     fun `countUnconnectedSides should work for sample input`() {
         val points = parse(sampleInput)
         assert(countUnconnectedSides(points) == 64)
+    }
+
+    @Test
+    fun `isInteriorPoint should return false if the point can reach the outside of the bounding box, given a potential point and a set of points`() {
+
+        /*
+        // looks like this, with a solid floor at z=0
+        // and a ceiling with a hole in it at z=3
+            XXXX
+            X..X
+            X..X
+            XXXX
+         */
+        
+        val boxFloor = (0..4).flatMap { x ->
+            (0..4).map { y -> Point3D(x,y,0)}
+        }.toSet()
+        val hollowExteriorLevel1 = setOf(
+            Point3D(0,0,1),
+            Point3D(1,0,1),
+            Point3D(2,0,1),
+            Point3D(3,0,1),
+            Point3D(0,1,1),
+            Point3D(3,1,1),
+            Point3D(0,2,1),
+            Point3D(3,2,1),
+            Point3D(0,3,1),
+            Point3D(1,3,1),
+            Point3D(2,3,1),
+            Point3D(3,3,1),
+        )
+        val hollowExteriorLevel2 = hollowExteriorLevel1.map { (x,y, _) -> Point3D(x,y,2) }.toSet()
+
+        val boxCeiling = boxFloor.map { (x,y, _) -> Point3D(x,y,3) }.toSet()
+        val missingCeilingPiece = Point3D(2,2,3)
+
+        val points = boxFloor + hollowExteriorLevel1 + hollowExteriorLevel2 + (boxCeiling - missingCeilingPiece)
+
+        val potentialPoint = Point3D(1,1,1)
+
+        val result = isInteriorPoint(potentialPoint, points)
+        assert(!result)
+    }
+
+    @Test
+    fun `isInteriorPoint should return true if the point cannot reach the outside of the bounding box, given a potential point and a set of points`() {
+        /*
+        // looks like this, with a solid floor at z=0
+        // and a ceiling at z=3
+            XXXX
+            X..X
+            X..X
+            XXXX
+         */
+        val boxFloor = (0..4).flatMap { x ->
+            (0..4).map { y -> Point3D(x,y,0)}
+        }.toSet()
+        val hollowExteriorLevel1 = setOf(
+            Point3D(0,0,1),
+            Point3D(1,0,1),
+            Point3D(2,0,1),
+            Point3D(3,0,1),
+            Point3D(0,1,1),
+            Point3D(3,1,1),
+            Point3D(0,2,1),
+            Point3D(3,2,1),
+            Point3D(0,3,1),
+            Point3D(1,3,1),
+            Point3D(2,3,1),
+            Point3D(3,3,1),
+        )
+        val hollowExteriorLevel2 = hollowExteriorLevel1.map { (x,y, _) -> Point3D(x,y,2) }.toSet()
+
+        val boxCeiling = boxFloor.map { (x,y, _) -> Point3D(x,y,3) }.toSet()
+
+        val points = boxFloor + hollowExteriorLevel1 + hollowExteriorLevel2 + boxCeiling
+
+        val potentialPoint = Point3D(1,1,1)
+
+        val result = isInteriorPoint(potentialPoint, points)
+        assert(result)
+    }
+
+    @Test
+    fun `findInteriorPoints should return interior points from a sealed structure`() {
+        /*
+        // looks like this, with a solid floor at z=0
+        // and a ceiling at z=3
+            XXXX
+            X..X
+            X..X
+            XXXX
+         */
+        val boxFloor = (0..4).flatMap { x ->
+            (0..4).map { y -> Point3D(x,y,0)}
+        }.toSet()
+        val hollowExteriorLevel1 = setOf(
+            Point3D(0,0,1),
+            Point3D(1,0,1),
+            Point3D(2,0,1),
+            Point3D(3,0,1),
+            Point3D(0,1,1),
+            Point3D(3,1,1),
+            Point3D(0,2,1),
+            Point3D(3,2,1),
+            Point3D(0,3,1),
+            Point3D(1,3,1),
+            Point3D(2,3,1),
+            Point3D(3,3,1),
+        )
+        val hollowExteriorLevel2 = hollowExteriorLevel1.map { (x,y, _) -> Point3D(x,y,2) }.toSet()
+        val boxCeiling = boxFloor.map { (x,y, _) -> Point3D(x,y,3) }.toSet()
+        val points = boxFloor + hollowExteriorLevel1 + hollowExteriorLevel2 + boxCeiling
+
+        val expectedInteriorPoints = setOf(
+            Point3D(1,1,1),
+            Point3D(2,1,1),
+            Point3D(1,2,1),
+            Point3D(2,2,1),
+            Point3D(1,1,2),
+            Point3D(2,1,2),
+            Point3D(1,2,2),
+            Point3D(2,2,2),
+        )
+
+        val interiorPoints = findInteriorPoints(points)
+
+        assert (expectedInteriorPoints == interiorPoints)
+    }
+
+    @Test
+    fun `findInteriorPoints should return an empty set given a structure with a hole`() {
+        /*
+        // looks like this, with a solid floor at z=0
+        // and a ceiling at z=3
+            XXXX
+            X..X
+            X..X
+            XXXX
+         */
+        val boxFloor = (0..4).flatMap { x ->
+            (0..4).map { y -> Point3D(x,y,0)}
+        }.toSet()
+        val hollowExteriorLevel1 = setOf(
+            Point3D(0,0,1),
+            Point3D(1,0,1),
+            Point3D(2,0,1),
+            Point3D(3,0,1),
+            Point3D(0,1,1),
+            Point3D(3,1,1),
+            Point3D(0,2,1),
+            Point3D(3,2,1),
+            Point3D(0,3,1),
+            Point3D(1,3,1),
+            Point3D(2,3,1),
+            Point3D(3,3,1),
+        )
+        val hollowExteriorLevel2 = hollowExteriorLevel1.map { (x,y, _) -> Point3D(x,y,2) }.toSet()
+        val boxCeiling = boxFloor.map { (x,y, _) -> Point3D(x,y,3) }.toSet()
+        val missingCeilingPiece = Point3D(2,2,3)
+        val points = boxFloor + hollowExteriorLevel1 + hollowExteriorLevel2 + (boxCeiling - missingCeilingPiece)
+
+        val interiorPoints = findInteriorPoints(points)
+
+        assert (interiorPoints == emptySet<Point3D>())
+    }
+
+    @Test
+    fun `findInteriorPoints should return the specified point given the provided example`() {
+        val points = parse(sampleInput)
+        val result = findInteriorPoints(points)
+
+        assert(result == setOf(Point3D(2,2,5)))
     }
 
     @Nested
